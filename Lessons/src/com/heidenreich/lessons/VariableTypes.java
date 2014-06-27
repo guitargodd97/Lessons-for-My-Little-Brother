0d1e200b9861c5a75f9358e9fba3caf928ab00c6
Overlord <Overlord@192.168.1.101>
2014-06-26 20:38:50 -0400

    Started variable types

---
+++ Lessons/src/com/heidenreich/lessons/VariableTypes.java
@@ -0,0 +1,18 @@
+package com.heidenreich.lessons;
+
+public class VariableTypes {
+
+	public static void main(String args[]) {
+
+		// Primitives
+		boolean operator = true;
+		char symbol = '!';
+		int number = 10;
+
+		// Object
+		String word = "Robert";
+		String sentence = "I was walking down the street when I saw a goat!";
+
+	}
+
+}
