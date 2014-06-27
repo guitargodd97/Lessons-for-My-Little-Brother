cda16e32fa0871fe6a215ae62e5537ef365ff394
Overlord <Overlord@192.168.1.101>
2014-06-26 20:21:37 -0400

    Initial Commit (Lesson 1)

---
+++ Lessons/src/com/heidenreich/lessons/IntExamples.java
@@ -0,0 +1,69 @@
+package com.heidenreich.lessons;
+
+public class IntExamples {
+	public static void main(String[] args) {
+
+		// Create Variables
+		int x = 90;
+		int y = 50;
+		int z = 0;
+
+		// Set Z to a number
+		z = 443;
+		System.out.println(z);
+
+		// Set Z to a variable
+		z = x;
+		System.out.println(z);
+
+		// Addition
+		z = x + y;
+		System.out.println(z);
+
+		// Subtraction
+		z = x - y;
+		System.out.println(z);
+
+		// Multiplication
+		z = x * y;
+		System.out.println(z);
+
+		// Division
+		z = x / y;
+		System.out.println(z);
+
+		// Modulus
+		z = x % y;
+		System.out.println(z);
+
+		// Increment
+		z = 5;
+		z++;
+		System.out.println(z);
+
+		// Simple Addition
+		z = 5;
+		z += 3;
+		System.out.println(z);
+
+		// Decrement
+		z = 5;
+		z--;
+		System.out.println(z);
+
+		// Simple Subtraction
+		z = 5;
+		z -= 3;
+		System.out.println(z);
+
+		// Simple Multiplication
+		z = 5;
+		z *= 19;
+		System.out.println(z);
+
+		// Simple Division
+		z = 6;
+		z /= 2;
+		System.out.println(z);
+	}
+}
---
+++ Lessons/src/com/heidenreich/tests/Robert1_YearCalculator.java
@@ -0,0 +1,9 @@
+package com.heidenreich.tests;
+
+public class Robert1_YearCalculator {
+	public static void main(String[] args) {
+		int myAge = 7;
+		int year = 2014;
+		System.out.println(year - myAge);
+	}
+}
